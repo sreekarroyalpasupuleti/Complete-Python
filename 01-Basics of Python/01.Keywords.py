ff78e9466142f0e122f5f155bcb9c19029f313f4
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-27 13:03:51 -0400

    Keywords are the reserved words in Python and can't be used as an identifier

--- 01-Basics of Python/01.Keywords.py
+++ 01-Basics of Python/01.Keywords.py
@@ -1,10 +1,14 @@
+# Keywords are the reserved words in Python and can't be used as an identifier
+# There are total 35 keywords are available in python
+# All keywords in Python contain only alphabets.
+# These are in lowercase except 3 keywords, True, False & None
 import keyword
 print(keyword.kwlist) # List all Python Keywords
-##output: ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class','continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global','if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return','try', 'while', 'with', 'yield']
+# output: ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class','continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global','if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return','try', 'while', 'with', 'yield']
 
 print(len(keyword.kwlist)) # len() --we can find the number values by using this function
-#output:35
+# output:35
 
 a = keyword.kwlist
 print(type(a))
-#output:<class 'list'>
+# output:<class 'list'>
