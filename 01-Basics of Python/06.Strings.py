68599c2f96d9784faf0c756878d86352b7137260
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-27 02:23:23 -0400

    String Creation

---
+++ 01-Basics of Python/06.Strings.py
@@ -0,0 +1,19 @@
+#String Creation
+a="SREEKAR NEWYORK" #String created with double quote.Space is also counted.(s=0,r=1,e=2,e=3,k=4 etc.)
+print(a)
+a='SREEKAR NEWYORK' #String created with single quote.
+print(a)
+print(a[0])# First character in string "a":Output is "S"
+print(a[1])# Second character in string "a":Output is "R"
+print(a[2])# Third character in string "a":Output is "E"
+print(a[3])# Fourth character in string "a":Output is "E"
+print(a[4])# Fifth character in string "a":Output is "K"
+print(a[5])# Six character in string "a":Output is "A"
+print(a[6])# Seventh character in string "a":Output is "R"
+print(a[7])# Eight character in string "a":Output is
+print(a[8])# Night character in string "a":Output is "N"
+print(a[9])# Tenth character in string "a":Output is "E"
+
+print(a[-1])# Last character in string "a":Output is "K"
+print(a[-2])# #Fetch Fourteenth element of the string "a":Output is "R"
+print(a[-3])# #Fetch Fifthteenth element of the string "a":Output is "O"
