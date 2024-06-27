c08d8ec9aca627fd3c31eece6443311c00293415
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-27 16:22:50 -0400

    Print the list of all Python keywords

--- 01-Basics of Python/01.Keywords.py
+++ 01-Basics of Python/01.Keywords.py
@@ -2,12 +2,18 @@
 # There are total 35 keywords are available in python
 # All keywords in Python contain only alphabets.
 # These are in lowercase except 3 keywords, True, False & None
+
+# Print the list of all Python keywords
 import keyword
 print(keyword.kwlist) # List all Python Keywords
 # output: ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class','continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global','if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return','try', 'while', 'with', 'yield']
 
+
+# Print the total number of keywords in Python
 print(len(keyword.kwlist)) # len() --we can find the number values by using this function
 # output:35
+
+# Assign keyword list to variable 'a' and print its type
 
 a = keyword.kwlist
 print(type(a))
