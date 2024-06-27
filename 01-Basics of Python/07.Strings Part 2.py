62b5d599d0b5e914d7bfbdc9283efce5a715f71a
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-27 12:05:54 -0400

    String Conatenation, Partitioning etc.

--- 01-Basics of Python/07.Strings Part 2.py
+++ 01-Basics of Python/07.Strings Part 2.py
@@ -1,12 +1,14 @@
 # String concatenation
-a1 = "Good"
-c2 = "Morning"
-v3 = a1 + c2
+a1 = "Good" #a1 is a string
+c2 = "Morning" #c2 is a string
+v3 = a1 + c2 #concatenation
 print(v3)
 
-q="Have"
-w="A"
-e="Good"
-r="Day"
+q="Have" # q is a string
+w="A" # w is a string
+e="Good" # e is a string
+r="Day" # r is a string
 t=q + " " + w + " " + e + " " + r
 print(t)
+#Iterating through a String
+string = "NEWYORK CITY" # string q is a string
