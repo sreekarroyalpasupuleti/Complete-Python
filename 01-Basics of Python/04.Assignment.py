3d55f67bfd0e572812e3597bced9113e94940e5f
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-26 22:07:44 -0400

    Assignment

---
+++ 01-Basics of Python/04.Assignment.py
@@ -0,0 +1,13 @@
+#Variable Assignment
+integer = 99 # Integer variable
+float = 19.777 # Float Variable
+string = "Welcome" # String variable
+print(integer)
+print(float)
+print(string)
+
+#Multiple Assignments
+integer , float , string = 79,8.32,"Welcome" # Using commas to separate
+print(integer)
+print(float)
+print(string)
