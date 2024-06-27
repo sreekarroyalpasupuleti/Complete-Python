4053653fc62f2ac1972c047476f502f318ac0303
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-26 21:34:25 -0400

    Python program name which can be used for identification purpose.

---
+++ 01-Basics of Python/02.Python Identifier.py
@@ -0,0 +1,25 @@
+#<1>Python Identifier
+A=5
+print(A)
+user=3
+print(user)
+
+#<2>Symbols are not identified in python
+#u$er=5 # #dentifier can't use special symbols except _
+
+#<3># Identifier can't start with a digit
+total21=30
+print(total21)
+#21total=30 #Digit rules identifier
+
+#<4>Identifiers are case sensitive
+hai=7
+HAI=6
+print(hai)
+print(HAI)
+
+#<5>Keywords cannot be assigned as identifier
+#true=1
+#else=5
+#false=6
+#if=8
