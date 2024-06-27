77278ad2f445721019c0c493e4cecb915c3cb7e8
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-26 21:48:23 -0400

    Comments can be used to explain the code

---
+++ 01-Basics of Python/03.Comments in Python.py
@@ -0,0 +1,24 @@
+# Single line comment
+a = 9
+
+# Multiple
+# line
+# comment
+salary = 10000
+print(salary)
+
+'''
+Multiple 
+line 
+comment
+'''
+laptop = 897
+print(laptop)
+
+"""
+Multiple 
+line 
+comment
+"""
+box = 965
+print(box)
