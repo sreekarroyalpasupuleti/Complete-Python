60326214e77d28330f738aa1d629c74d906c9238
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-26 21:08:26 -0400

    Print all the Python Keywords

--- 01-Basics of Python/01.Keywords.py
+++ 01-Basics of Python/01.Keywords.py
@@ -1,10 +1,7 @@
 import keyword
 print(keyword.kwlist) # List all Python Keywords
 
-##output: ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class',
-# 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global',
-# 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return',
-# 'try', 'while', 'with', 'yield']
+##output: ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class','continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global','if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return','try', 'while', 'with', 'yield']
 
 
 print(len(keyword.kwlist)) #len()--we can find the number values by using this function
