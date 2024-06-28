425dd35e50755718fc8a104e4c68e46ddfe3cabd
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-06-27 20:16:11 -0400

    string formatting

--- 01-Basics of Python/08.Strings Part 3.py
+++ 01-Basics of Python/08.Strings Part 3.py
@@ -1,3 +1,19 @@
+# There are three types to perform string formatting.
+
+# Type1. Using % Operator (Old method with placeholders):
+# The % operator is used to format strings by inserting values into placeholders.
+# It's an older method that is still found in some legacy codebases.
+
+# 1. Using % Operator (Old method with placeholders):
+# The % operator is used to format strings by inserting values into placeholders.
+a = ("shyam")
+v = 30
+formatted_string = "My name is %s. My age is %d." % (a, v)
+print(formatted_string)
+
+
+# Type2. Using .format() Method:
+# The .format() method provides more flexibility and readability compared to % formatting.
 # The basic syntax for using the .format() method
 # formatted_string = "String with {} placeholders and {}".format(value1, value2)----SYNTAX
 # Example usage of the .format() method
@@ -19,3 +35,17 @@ age = 56
 profession = "Data Scientist"
 code2 = "Hello, my name is {}. I am {} years old, and I work as an {}.".format(name, age, profession)
 print(code2)
+
+#Type3: Using f-strings (Formatted String Literals):
+#Introduced in Python 3.6, f-strings provide a concise and readable way to embed expressions inside string literals.
+# They are the most modern and preferred method of string formatting.
+name = "sreekar"
+age = 56
+formatted_string = f"Hello, {name}. You are {age} years old."
+print(formatted_string)
+
+# Final Summary:
+# Readability: f-strings (f"..." syntax) are generally considered the most readable and concise method.
+# Performance: f-strings are also faster compared to .format() and % formatting.
+# Legacy Compatibility: % formatting is still used in older codebases, while .format() is versatile and widely used in modern Python.
+
