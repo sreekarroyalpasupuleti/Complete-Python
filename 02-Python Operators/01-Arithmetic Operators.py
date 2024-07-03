e4985ba5f8f5899ab5d3bc9cabf082a04e29ebca
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-07-02 23:23:14 -0400

    Arithmetic Operators

---
+++ 02-Python Operators/01-Arithmetic Operators.py
@@ -0,0 +1,34 @@
+a = 77
+b = 22
+x = 'Hello'
+y = 'Hai'
+# Addition
+c = a + b
+print(c)
+print('Addition of {} and {} will give :- {}\n'.format(a,b,c))
+# Concatenate string using plus operator
+z = x+y
+print ('Concatenate string \'x\' and \'y\' using \'+\' operaotr :- {}\n'.format(z))
+
+# Subtraction
+c = a - b
+print('Subtracting {} from {} will give :- {}\n'.format(b,a,c))
+
+# Multiplication
+c = a * b
+print('Multiplying {} and {} will give :- {}\n'.format(a,b,c))
+
+# Division
+c = a / b
+print('Dividing {} by {} will give :- {}\n'.format(a,b,c))
+
+# Modulo of both number
+c = a % b
+print('Modulo of {} , {} will give :- {}\n'.format(a,b,c))
+
+# Power
+c = a ** b
+print('{} raised to the power {} will give :- {}\n'.format(a,b,c))
+# Division(floor)
+c = a // b
+print('Floor division of {} by {} will give :- {}\n'.format(a,b,c))
