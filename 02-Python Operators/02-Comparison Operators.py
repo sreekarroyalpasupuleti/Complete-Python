ba5f63ce02b8256f25d6fb454e9dd30979dcfa34
sreekar <sreekarroyalpasupuleti@gmail.com>
2024-07-02 23:26:50 -0400

    Comparison Operators

---
+++ 02-Python Operators/02-Comparison Operators.py
@@ -0,0 +1,10 @@
+//Comparison Operators
+
+x = 80
+y = 20
+print('Is x greater than y :- ',x>y)
+print('\nIs x less than y :- ',x<y)
+print('\nIs x equal to y :- ',x==y)
+print('\nIs x not equal to y :- ',x!=y)
+print('\nIs x greater than or equal to y :- ',x>=y)
+print('\nIs x less than or equal to y :- ',x<=y)
